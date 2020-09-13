90ffbfc7b4dbd65b4092177a5fab3f94491298fa
AdriSolid <biotekgis@gmail.com>
2020-09-13 15:18:01 +0200

    Add deploy files

---
+++ precache-manifest.dd5439d30f6a429b5e4202e67b6af27c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "326e1e8ffb994e862403c555fd1de2a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a93318fd9c336afa4084",
+    "url": "/static/css/2.f8095d06.chunk.css"
+  },
+  {
+    "revision": "a93318fd9c336afa4084",
+    "url": "/static/js/2.7539ccc8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7539ccc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e51eef409d1f83cba313",
+    "url": "/static/js/main.9b9281f0.chunk.js"
+  },
+  {
+    "revision": "72faa2d309c80cce18d4",
+    "url": "/static/js/runtime-main.6c8d31db.js"
+  }
+]);
